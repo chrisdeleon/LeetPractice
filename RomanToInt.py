7918739173dc9765c3490fdda3c49b9313a5d395
chrisdeleon <deleonc2050@gmail.com>
2024-09-13 14:10:31 -0500

    added match-case

--- RomanToInt.py
+++ RomanToInt.py
@@ -2,8 +2,21 @@
 
 class Solution:
     def romanToInt(self, s:str) -> int:
-        return 1
+        s = list(s) #saves string to list of each character in string
+        print(s)
+        sum = 0
+
+        #will probably want to catch any leading i's prior to case-matching
+
+        #iterate through every item in the list
+        for x in s:
+            match x:
+                case "i": 
+                    print("x is 1")
+
+
+        return sum
     
 
 solution_instance = Solution()
-result = solution_instance.romanToInt(x = "iii")
+result = solution_instance.romanToInt("iii")
