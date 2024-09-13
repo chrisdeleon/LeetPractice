e2803373ef5bd2ffcff83527d862b3bd8ba6c6cf
chrisdeleon <deleonc2050@gmail.com>
2024-09-13 13:38:18 -0500

    init commit

---
+++ RomanToInt.py
@@ -0,0 +1,9 @@
+# for a given roman numeral, return the integer equivalent
+
+class Solution:
+    def romanToInt(self, s:str) -> int:
+        return 1
+    
+
+solution_instance = Solution()
+result = solution_instance.romanToInt(x = "iii")
